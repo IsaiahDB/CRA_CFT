a0a59a5571a4e7ff146f5d54271457ad6d84063e
IsiahDB <IsaiahBanks500@gmail.com>
2022-06-04 16:41:12 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
+import NumberOfEvents from './NumberOfEvents';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents/>
       
     </div>
   );
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -3,18 +3,43 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
 
+
+
+// describe('<App /> component', () => {
+//   let AppWrapper;
+//   beforeAll(() => {
+//     AppWrapper = shallow(<App />);
+//   });
+
+//   test('render list of events', () => {
+//     expect(AppWrapper.find(EventList)).toHaveLength(1);
+//   });
+
+//   test('render CitySearch', () => {
+//     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+//   });
+// });
 describe('<App /> component', () => {
   let AppWrapper;
-  beforeAll(() => {
-    AppWrapper = shallow(<App />);
-  });
-
-  test('render list of events', () => {
-    expect(AppWrapper.find(EventList)).toHaveLength(1);
-  });
-
-  test('render CitySearch', () => {
-    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
-  });
+beforeAll(() => {
+  AppWrapper = shallow(<App />);
 });
+
+  //test rendering a list of events
+  test('render list of events', () => {
+      expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
+
+    //test rendering CitySearch
+    test('render CitySearch', () => {
+      expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+    });
+    
+    //test feature 2
+    test('render NumberOfEvents', () => {
+      expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+    });
+});
+
