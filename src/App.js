4d7f0cb0caba857d304fbf4f3196c87481ebdf13
IsiahDB <IsaiahBanks500@gmail.com>
2022-06-14 19:42:35 -0500

    failing test

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ getData = () => {
     
     return (
       <div className="App">
+        <h1>Meet App</h1>
       <CitySearch
         locations={this.state.locations}
         updateEvents={this.updateEvents}
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -77,7 +77,7 @@ describe('<App /> integration', () => {
     const AppWrapper = mount(<App />);
     const AppNumberOfEventsState = AppWrapper.state("numberOfEvents");
     expect(AppNumberOfEventsState).not.toEqual(undefined);
-    expect(AppWrapper.find(EventList).props().numberOfEvents).toEqual(32);
+    expect(AppWrapper.find(EventList).props().numberOfEvents).toEqual(undefined);
     AppWrapper.unmount();
 });
 
